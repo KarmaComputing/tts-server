1581939cd4c24f7ca9f0c63546b85053708e11b9
chrisjsimpson <chris.j.simpson@live.co.uk>
2022-05-23 19:01:45 +0100

    Fix #1 add initial post request -> wav

--- README.md
+++ README.md
@@ -10,6 +10,18 @@ TTS server using those models.
 
 ## Install / run
 
+Install server requirements (only run on server)
 ```
 ./install.sh
+```
+
+## Local development
+
+```
+python3 -m venv venv
+. venv/bin/activate
+pip install -r requirements.txt
+export FLASK_APP=app
+export FLASK_DEBUG=1
+flask run
 ```
---
+++ app.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+import os
+
+
+from flask import (
+    Flask,
+    render_template,
+    request,
+    send_from_directory,
+    redirect,
+    url_for,
+)
+import subprocess
+from time import time_ns
+
+load_dotenv(verbose=True)
+
+app = Flask(__name__)
+app.config.update(os.environ)
+
+UPLOADS_BATH_PATH = app.config.get("UPLOADS_BATH_PATH")
+
+
+@app.route("/download/<name>")
+def download_file(name):
+    return send_from_directory(f"{UPLOADS_BATH_PATH}/", name)
+
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    text_unsafe = request.form.get("text")
+    filename = f"{time_ns()}.wav"
+
+    # WARNING this is unsafe, see shell.escape
+    subprocess.run(
+        f'time tts --text "{text_unsafe}" --out_path "{UPLOADS_BATH_PATH}/{filename}" --model_name tts_models/en/ljspeech/fast_pitch',  # noqa: E501
+        shell=True,
+    )
+    return redirect(url_for("download_file", name=filename))
