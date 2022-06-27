a2586166bef3bd4f703f6379c0c2faf970db7582
chrisjsimpson <chris.j.simpson@live.co.uk>
2022-06-27 19:35:36 +0100

    #14 support request language in api call
    example call curl -L 'https://api.example.com/submit?lang=de'   --data-raw 'text=Hallo+wie+gehts+es+dir?'   --compressed > out.wav

--- app.py
+++ app.py
@@ -15,12 +15,29 @@ from time import time_ns
 
 load_dotenv(verbose=True)
 
-supported_languages = ["en", "de", "zh-cn"]
+supported_languages = ["en", "de", "fe", "zh-cn", "es", "nl", "tr", "it", "ja"]
+
+# Map the ISO 639-1 language code to a model which supports that language
+# Note: Some languages have multiple models to choose from, this is simpler
+# on purpose to satisfy https://github.com/KarmaComputing/tts-server/issues/14
+# and can be extended in future
+language_to_model = {
+    "de": "tts_models/de/thorsten/tacotron2-DCA",
+    "en": "tts_models/de/thorsten/tacotron2-DCA",
+    "fe": "tts_models/fr/mai/tacotron2-DDC",
+    "zh-CN": "tts_models/zh-CN/baker/tacotron2-DDC-GST",
+    "es": "tts_models/es/mai/tacotron2-DDC",
+    "nl": "tts_models/nl/mai/tacotron2-DDC",
+    "tr": "tts_models/tr/common-voice/glow-tts",
+    "it": "tts_models/it/mai_female/glow-tts",
+    "ja": " 19: tts_models/ja/kokoro/tacotron2-DDC",
+}
 
 app = Flask(__name__)
 app.config.update(os.environ)
 
 UPLOADS_BATH_PATH = app.config.get("UPLOADS_BATH_PATH")
+TTS_PATH = app.config.get("TTS_PATH")
 
 
 @app.route("/download/<name>")
@@ -42,16 +59,20 @@ def submit():
     # Get different language model if possible
     if request.args.get("lang"):
         language = request.args.get("lang").lower()
+        print(f"Attemping to use language {language}")
         if language in supported_languages:
-            # wip default to de
-            # see github.com/KarmaComputing/tts-server/issues/14
-            model_name = "tts_models/de/thorsten/tacotron2-DCA"
+            model_name = language_to_model[language]
+            print(f"Model name set to: {model_name}")
+        else:
+            print(f"Could not locate model for requested language: {language}")
+
+    print(f"Model name is set to: {model_name}")
 
     filename = f"{time_ns()}.wav"
 
     # WARNING this is unsafe, see shell.escape
     subprocess.run(
-        f'tts --text "{text_unsafe}" --out_path "{UPLOADS_BATH_PATH}/{filename}" --model_name {model_name}',  # noqa: E501
+        f'{TTS_PATH} --text "{text_unsafe}" --out_path "{UPLOADS_BATH_PATH}/{filename}" --model_name {model_name}',  # noqa: E501
         shell=True,
     )
     return redirect(url_for("download_file", name=filename))
